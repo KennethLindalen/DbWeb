cac179a62f782a967d8a6da7c8b489589fb25f67
Krunchos <kennethlindalen@gmail.com>
2018-01-25 10:32:32 +0100

    Oppsatt tabell

--- sider/statistikk.php
+++ sider/statistikk.php
@@ -24,7 +24,6 @@
           <li><a href="#">Nyheter</a></li>
           <li><a href="#">Aktiviteter</a></li>
           <li><a href="#">Spilloversikt</a></li>
-          <li><a href="#">Kontakt</a></li>
           <li><a href="#">Bli medlem</a></li>
           <li><a href="#">Logg inn</a></li>
         </ul>
@@ -32,11 +31,69 @@
     </nav>
 
 
-    <div class="Container">
-      <div class="content">
+    <main style ="text-align:center;">
+      <BR /><BR />
+      <H1>Spilloversikt</H1>
+      <p>
+        Her kan man se resultater og informasjon om sportsarrangementer som Nedelaget Idrettsklubb er involvert i.
+      </p>
 
+      <h2>Fotball</h2>
+      <div style="padding: 5px;">
+        <table class="table">
+          <thead>
+            <th scope="col">Dato </th>
+            <th scope="col">Hjemme </th>
+            <th scope="col">Poeng </th>
+            <th scope="col">Borte </th>
+          </thead>
+        </table>
       </div>
-    </div>
+      <h2>Hockey</h2>
+      <div style="padding: 5px;">
+        <table class="table">
+          <thead>
+            <th scope="col">Dato </th>
+            <th scope="col">Hjemme </th>
+            <th scope="col">Poeng </th>
+            <th scope="col">Borte </th>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">19.10.2018</th>
+              <td>Nederlaget Idrettsklubb</td>
+              <td>7 - 9</td>
+              <td>Kjosen IL</td>
+            </tr>
+            <tr>
+              <th scope="row">12.04.2017</th>
+              <td>Drangedal</td>
+              <td>6 - 3</td>
+              <td>Kjosen IL</td>
+            </tr>
+            <tr>
+              <th scope="row">17.06.2017</th>
+              <td>Nederlaget Idrettsklubb</td>
+              <td>2 - 9</td>
+              <td>Dalen IL</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <h2>Golf</h2>
+      <div style="padding: 5px;">
+        <table class="table">
+          <thead>
+            <th scope="col">JEG </th>
+            <th scope="col">VET </th>
+            <th scope="col">IKKE </th>
+            <th scope="col">HVORDAN </th>
+            <th scope="col">GOLFSCORE </th>
+            <th scope="col">SER UT </th>
+          </thead>
+        </table>
+      </div>
+    </main>
 
   </body>
 </html>
