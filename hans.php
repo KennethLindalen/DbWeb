3f2cae08239891caa7f67b731c4ca4f3d3c99e6c
svgnAyure <kristian.sta@gmail.com>
2018-01-29 18:37:44 +0100

    hans

---
+++ hans.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+  <body>
+    <h1>Vælkommin</h1>
+  <form action="skjemadata.php" method="post">
+<p>
+<input type="text" name="brukernavn" placeholder="brukernavn">
+<br><br>
+<input type="password" name="password"
+placeholder="passord" name="passord">
+<br>
+<br>
+<input type="submit" value="submit" name="knapp">
+</p>
+</form>
+  </body>
+</html>
---
+++ skjemadata.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Velkommen</title>
+  </head>
+  <body>
+    <h1>Velkommen</h1>
+
+<?php
+$brukernavn = $_POST['brukernavn'];
+print "<p>Velkommen ${brukernavn}</p>";
+
+?>
+  </body>
+</html>
