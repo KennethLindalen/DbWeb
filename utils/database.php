3c7d664cf78f9a5f53e5e9c14bf26adba8ff1ce9
svgnAyure <kristian.sta@gmail.com>
2018-02-02 15:00:36 +0100

    test av envvars

--- utils/database.php
+++ utils/database.php
@@ -1,5 +1,13 @@
 <?php
 
-  
+  function getConnection() {
+    $hostname = "";
+    $username = "";
+    $password = "";
+    $database = "";
+    return mysqli_connect($hostname, $username, $password, $database);
+  }
+
+  echo $_ENV["TEST"];
 
 ?>
