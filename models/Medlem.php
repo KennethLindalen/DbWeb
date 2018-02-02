5138f4f9a413f83f1ba12a70318e56195fa2449f
svgnAyure <kristian.sta@gmail.com>
2018-02-02 17:28:35 +0100

    bcrypt

--- models/Medlem.php
+++ models/Medlem.php
@@ -2,16 +2,20 @@
 
 class Medlem {
 
-
+  private $medlemsnummer;
+  private $fornavn;
+  private $etternavn;
 
   public static function søk() {
 
   }
 
   public function lagre() {
-
+    query("INSERT INTO faen");
   }
 
 }
+
+  echo password_hash("passord", PASSWORD_BCRYPT);
 
 ?>
