d29b2ffe7d81cb39ee0126caaa041d42c12b0583
svgnAyure <kristian.sta@gmail.com>
2018-02-04 18:54:20 +0100

    test

--- utils/registrering.php
+++ utils/registrering.php
@@ -6,6 +6,6 @@
     $medlem = new Medlem()
   }
 
-  $medlem = new Medlem(123, "Kristian", "Stang", "Romnesvegen 81", 3830, "kristian.sta@gmail.com", "pass")
+  $medlem = new Medlem(123, "Kristian", "Stang", "Romnesvegen 81", 3830, "kristian.sta@gmail.com", "pass");
   $medlem->lagre();
 ?>
