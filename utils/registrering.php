457c96606fcadd7dea3ced60c58e5e643f7fd654
svgnAyure <kristian.sta@gmail.com>
2018-02-04 18:55:28 +0100

    test

--- utils/registrering.php
+++ utils/registrering.php
@@ -1,13 +1,11 @@
 <?php
 
-  //include_once "../models/Medlem.php";
+  include_once "../models/Medlem.php";
 
   function registrerMedlem() {
-    $medlem = new Medlem()
+    
   }
 
-  //$medlem = new Medlem(123, "Kristian", "Stang", "Romnesvegen 81", 3830, "kristian.sta@gmail.com", "pass");
-  //$medlem->lagre();
-
-  echo "hei";
+  $medlem = new Medlem(123, "Kristian", "Stang", "Romnesvegen 81", 3830, "kristian.sta@gmail.com", "pass");
+  $medlem->lagre();
 ?>
