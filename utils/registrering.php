ec9b1cb915136ed834cdbef1ad084c456a1604d3
svgnAyure <kristian.sta@gmail.com>
2018-02-04 18:51:49 +0100

    test

--- utils/registrering.php
+++ utils/registrering.php
@@ -3,7 +3,9 @@
   include_once "../models/Medlem.php";
 
   function registrerMedlem() {
-    $medlem = new Medlem
+    $medlem = new Medlem()
   }
 
+  $medlem = new Medlem(123, "Kristian", "Stang", "Romnesvegen 81", 3830, "kristian.sta@gmail.com", "pass")
+  $medlem->lagre();
 ?>
