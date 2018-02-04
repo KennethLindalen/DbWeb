0f4b274a65a641404bf9869e7071ce7b41de8b6f
svgnAyure <kristian.sta@gmail.com>
2018-02-04 20:51:03 +0100

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include_once "models/Medlem.php";
 
-$medlem = new Medlem("Kristian", "Stang", "Romnesvegen 81", 3830, "kristian.sta@gmail.com", "pass");
+$medlem = new Medlem("Kristian", "Stang", "Romnesvegen 81", "3830", "kristian.sta@gmail.com", "pass");
 $medlem->lagre();
 
 ?>
