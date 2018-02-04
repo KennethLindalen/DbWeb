f7a7279f03f2a84c9bbd188e4a34421d7df97d9f
svgnAyure <kristian.sta@gmail.com>
2018-02-04 20:47:10 +0100

    test

--- models/Medlem.php
+++ models/Medlem.php
@@ -28,7 +28,7 @@ class Medlem {
 
     $conn = getConnection();
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssssss",
+    /*$stmt->bind_param("ssssss",
       $this->fornavn,
       $this->etternavn,
       $this->adresse,
