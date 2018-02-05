e06ea4f93152f9ac38aefd69fbb64d8c1f968888
svgnAyure <kristian.sta@gmail.com>
2018-02-05 03:53:40 +0100

    test

--- utils/database.php
+++ utils/database.php
@@ -13,6 +13,8 @@ class Database {
   }
 
   public function getRow($query, $params = []) {
+    var_dump($query);
+    var_dump($params);
     $stmt = $this->conn->prepare($query);
     $stmt->bind_param("s", ...$params);
     $stmt->execute();
