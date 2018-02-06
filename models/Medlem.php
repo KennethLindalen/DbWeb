b589dae76d7d6ca02cfc41e2c2794f8480792bd8
Kristian Stang <kristian.sta@gmail.com>
2018-02-06 10:54:18 +0100

    test

--- models/Medlem.php
+++ models/Medlem.php
@@ -1,34 +1,37 @@
 <?php
 
 include_once "utils/database.php";
-include_once "models/Poststed.php";
 
 class Medlem {
 
   public function __construct($medlem = [], $fraDatabase = false) {
     $this->medlemsnummer = $medlem["medlemsnummer"];
-    $this->fornavn = $medlem["fornavn"];
-    $this->etternavn = $medlem["etternavn"];
-    $this->adresse = $medlem["adresse"];
-    $this->postnummer = $medlem["postnummer"];
+    $this->fornavn       = $medlem["fornavn"];
+    $this->etternavn     = $medlem["etternavn"];
+    $this->adresse       = $medlem["adresse"];
+    $this->postnummer    = $medlem["postnummer"];
     $this->telefonnummer = $medlem["telefonnummer"];
-    $this->epost = $medlem["epost"];
-    $this->passord = $medlem["passord"];
+    $this->epost         = $medlem["epost"];
+    $this->passord       = $medlem["passord"];
     if (!$fraDatabase) $this->valider();
   }
 
   private function valider($medlem) {
-    // Sørg for at hashede passord validerer til true
     // hvis gyldig, krypter passord
-    echo "i valider";
-  }
-
-  public static function finn() {
-
+    // hvis ugyldig, kast unntak
+    $this->passord = password_hash($this->passord, PASSWORD_BCRYPT);
   }
 
   public function lagre() {
+    // insert into medlem med prepared statement eller stored procedure
+  }
 
+  public static function finn() {
+    // finn medlem med gitt medlemsnummer eller epost
+  }
+
+  public static function finnPassord() {
+    // hent hashet passord for innlogging
   }
 
 }
