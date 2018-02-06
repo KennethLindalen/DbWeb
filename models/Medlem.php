6e607abe99d794a01818945fa827a37f35c639ed
Kristian Stang <kristian.sta@gmail.com>
2018-02-06 10:08:04 +0100

    fjerna lagre-metode

--- models/Medlem.php
+++ models/Medlem.php
@@ -25,16 +25,7 @@ class Medlem {
   }
 
   public function lagre() {
-    $sql = "INSERT INTO Medlem (`Fornavn`, `Etternavn`, `Adresse`, `Postnummer`, `Telefonnummer`, `E-postadresse`, `Passord`) "
-         . "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-    $conn = getConnection();
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssssss", $this->fornavn, $this->etternavn, $this->adresse, $this->postnummer, $this->telefonnummer, $this->epost, $this->passord);
-    $stmt->execute();
-    var_dump($conn);
-    $stmt->close();
-    $conn->close();
   }
 
 }
