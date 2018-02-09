0f91cdcabfe4baadacf4ef74f398350b616b2e6d
Krunchos <kennethlindalen@gmail.com>
2018-02-09 10:04:37 +0100

    Fikset modaler på spilloversikt.php

--- p/spilloversikt.php
+++ p/spilloversikt.php
@@ -1,122 +1,51 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Nederlaget IK - spilloversikt</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bungee+Hairline|Open+Sans+Condensed:300|Open+Sans">
-    <link rel="stylesheet" type="text/css" href="../css/layout.css">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <script type="javascript "src="../skript/modal.js"></script>
 
-  </head>
-  <body>
+<head>
+	<meta charset="utf-8">
+	<title>Nederlaget IK</title>
+	<link href="https://fonts.googleapis.com/css?family=Bungee+Hairline|Open+Sans+Condensed:300|Open+Sans" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet" type="text/css">
+	<link href="../css/modal.css" rel="stylesheet" type="text/css">
+	<script src="../skript/modal.js"></script>
+</head>
 
-    <header>
-      <div class="container">
-        <p>Logo</p>
-        <h1><a href ="../index.php">Nederlaget<br>Idrettsklubb</a></h1>
-      </div>
-    </header>
+<body>
+	<header>
+		<div class="container">
+			<p>Logo</p>
+			<h1><a href="../index.php">Nederlaget<br>
+			Idrettsklubb</a>
+			</h1>
+		</div>
+		<!-- Slutt på header container div -->
+	</header>
+	<nav>
+		<div class="container">
+			<ul>
+				<li>	<a href="nyheter.php">Nyheter</a>
+				</li>
+				<li>	<a href="aktiviteter.php">Aktiviteter</a>
+				</li>
+				<li>	<a href="spilloversikt.php">Spilloversikt</a>
+				</li>
+				<li>	<a href="#" onclick="openRegistrerModal()">Bli medlem</a>
+				</li>
+				<li>	<a href="#" onclick="openLoginModal()">Logg inn</a>
+				</li>
+			</ul>
+		</div>
+		<!-- Slutt på nav container div -->
+	</nav>
+	<main>
+    <div class="container">
+      <h1>spilloversikt</h1>
+      <p>
+        Her kan man se spilloversikt og informasjon om sportsarrangementer som Nedelaget Idrettsklubb er involvert i
+      </p>
+    </div>
 
-    <nav>
-      <div class="container">
-        <ul>
-          <li><a href="nyheter.php">Nyheter</a></li>
-          <li><a href="aktiviteter.php">Aktiviteter</a></li>
-          <li><a href="spilloversikt.php">Spilloversikt</a></li>
-          <li><a href="#">Bli medlem</a></li>
-          <li><a href="#">Logg inn</a></li>
-        </ul>
-      </div>
-    </nav>
-
-    <main>
-      <div class="container">
-        <h1>spilloversikt</h1>
-        <p>
-          Her kan man se spilloversikt og informasjon om sportsarrangementer som Nedelaget Idrettsklubb er involvert i
-        </p>
-      </div>
-
-      <div class="container">
-        <h2>Fotball</h2>
-        <div style="padding: 5px;">
-          <table class="table" style="border: solid 1px #e0e0e0">
-            <thead>
-              <th scope="col">Dato </th>
-              <th scope="col">Hjemme </th>
-              <th scope="col">Poeng </th>
-              <th scope="col">Borte </th>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">19.10.2018</th>
-                <td>Nederlaget Idrettsklubb</td>
-                <td>7 - 9</td>
-                <td>Kjosen IL</td>
-              </tr>
-              <tr>
-                <th scope="row">12.04.2017</th>
-                <td>Drangedal</td>
-                <td>6 - 3</td>
-                <td>Kjosen IL</td>
-              </tr>
-              <tr>
-                <th scope="row">17.06.2017</th>
-                <td>Nederlaget Idrettsklubb</td>
-                <td>2 - 9</td>
-                <td>Dalen IL</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <h2>Hockey</h2>
-        <div style="padding: 5px;">
-          <table class="table" style="border: solid 1px #e0e0e0">
-            <thead>
-              <th scope="col">Dato </th>
-              <th scope="col">Hjemme </th>
-              <th scope="col">Poeng </th>
-              <th scope="col">Borte </th>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">19.10.2018</th>
-                <td>Nederlaget Idrettsklubb</td>
-                <td>7 - 9</td>
-                <td>Kjosen IL</td>
-              </tr>
-              <tr>
-                <th scope="row">12.04.2017</th>
-                <td>Drangedal</td>
-                <td>6 - 3</td>
-                <td>Kjosen IL</td>
-              </tr>
-              <tr>
-                <th scope="row">17.06.2017</th>
-                <td>Nederlaget Idrettsklubb</td>
-                <td>2 - 9</td>
-                <td>Dalen IL</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <h2>Golf</h2>
-        <div style="padding: 5px;">
-          <table class="table" style="border: solid 1px #e0e0e0">
-            <thead>
-              <th scope="col">JEG </th>
-              <th scope="col">VET </th>
-              <th scope="col">IKKE </th>
-              <th scope="col">HVORDAN </th>
-              <th scope="col">GOLFSCORE </th>
-              <th scope="col">SER UT </th>
-            </thead>
-          </table>
-        </div>
-
+    <div class="container">
       <h2>Fotball</h2>
       <div style="padding: 5px;">
         <table class="table" style="border: solid 1px #e0e0e0">
@@ -148,6 +77,7 @@
           </tbody>
         </table>
       </div>
+
       <h2>Hockey</h2>
       <div style="padding: 5px;">
         <table class="table" style="border: solid 1px #e0e0e0">
@@ -168,7 +98,7 @@
               <th scope="row">12.04.2017</th>
               <td>Drangedal</td>
               <td>6 - 3</td>
-              <td>Nederlaget Idrettsklubb</td>
+              <td>Kjosen IL</td>
             </tr>
             <tr>
               <th scope="row">17.06.2017</th>
@@ -179,6 +109,7 @@
           </tbody>
         </table>
       </div>
+
       <h2>Golf</h2>
       <div style="padding: 5px;">
         <table class="table" style="border: solid 1px #e0e0e0">
@@ -192,67 +123,147 @@
           </thead>
         </table>
       </div>
-      </div>
-    </main>
-    <footer>
 
-    </footer>
+    <h2>Fotball</h2>
+    <div style="padding: 5px;">
+      <table class="table" style="border: solid 1px #e0e0e0">
+        <thead>
+          <th scope="col">Dato </th>
+          <th scope="col">Hjemme </th>
+          <th scope="col">Poeng </th>
+          <th scope="col">Borte </th>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">19.10.2018</th>
+            <td>Nederlaget Idrettsklubb</td>
+            <td>7 - 9</td>
+            <td>Kjosen IL</td>
+          </tr>
+          <tr>
+            <th scope="row">12.04.2017</th>
+            <td>Drangedal</td>
+            <td>6 - 3</td>
+            <td>Kjosen IL</td>
+          </tr>
+          <tr>
+            <th scope="row">17.06.2017</th>
+            <td>Nederlaget Idrettsklubb</td>
+            <td>2 - 9</td>
+            <td>Dalen IL</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <h2>Hockey</h2>
+    <div style="padding: 5px;">
+      <table class="table" style="border: solid 1px #e0e0e0">
+        <thead>
+          <th scope="col">Dato </th>
+          <th scope="col">Hjemme </th>
+          <th scope="col">Poeng </th>
+          <th scope="col">Borte </th>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">19.10.2018</th>
+            <td>Nederlaget Idrettsklubb</td>
+            <td>7 - 9</td>
+            <td>Kjosen IL</td>
+          </tr>
+          <tr>
+            <th scope="row">12.04.2017</th>
+            <td>Drangedal</td>
+            <td>6 - 3</td>
+            <td>Nederlaget Idrettsklubb</td>
+          </tr>
+          <tr>
+            <th scope="row">17.06.2017</th>
+            <td>Nederlaget Idrettsklubb</td>
+            <td>2 - 9</td>
+            <td>Dalen IL</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <h2>Golf</h2>
+    <div style="padding: 5px;">
+      <table class="table" style="border: solid 1px #e0e0e0">
+        <thead>
+          <th scope="col">JEG </th>
+          <th scope="col">VET </th>
+          <th scope="col">IKKE </th>
+          <th scope="col">HVORDAN </th>
+          <th scope="col">GOLFSCORE </th>
+          <th scope="col">SER UT </th>
+        </thead>
+      </table>
+    </div>
+    </div>
+	</main>
+	<footer>
+		<div class="container">
+			<p>Hey</p>
+			<p>Halla</p>
+		</div>
+		<!-- Slutt på footer container div -->
+	</footer>
+	<div class="modal" id="loginModal">
+		<div class="modal-content">
+			<div class="modal-header">	<span class="closeBtn"></span>
+				<h2>Logg inn</h2>
+				<h6>Trykk på utsiden av boksen for å lukke</h6>
+			</div>
+			<!-- Slutt på modal-header div -->
+			<div class="modal-body">
+				<h4>Brukernavn(e-post)</h4>
+				<input name="loggInnBrukernavn" type="text" value="">
+				<br>
+				<br>
+				<h5>Passord</h5>
+				<input name="loggInnPassord" type="password" value="">
+			</div>
+			<!-- Slutt på modal-body div -->
+			<div class="modal-footer">
+				<button class="loggInnKnapp" name="LoggInnBtn" type="button">Logg Inn</button>
+			</div>
+			<!-- Slutt på modal-footer div -->
+		</div>
+		<!-- Slutt på modal-content div -->
+	</div>
+	<!-- Slutt på loginModal div -->
+	<div class="modal" id="registrerModal">
+		<div class="modal-content">
+			<div class="modal-header">	<span class="closeBtn"></span>
+				<h2>Registrer deg</h2>
+				<h6>Trykk på utsiden av boksen for å lukke</h6>
+			</div>
+			<!-- Slutt på modal-header div -->
+			<div class="modal-body">
+				<h4>Brukernavn(e-post)</h4>
+				<input name="RegistrerBrukernavn" type="text" value="">
+				<br>
+				<h4>Epost</h4>
+				<input name="RegistrerBrukernavn" type="text" value="">
+				<br>
+				<h4>Passord</h4>
+				<input name="RegistrerPassord" type="password" value="">
+				<br>
+				<h4>Passord igjen</h4>
+				<input name="RegistrerPassordIgjen" type="password" value="">
+			</div>
+			<!-- Slutt på modal-body div -->
+			<div class="modal-footer">
+				<button class="RegKnapp" name="RegKnapp" type="button">Registrer deg</button>
+			</div>
+			<!-- Slutt på modal-footer div -->
+		</div>
+		<!-- Slutt på modal-content div -->
+	</div>
+	<!-- Slutt på registrerModal div -->
+	<script crossorigin="anonymous" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8" src="http://code.jquery.com/jquery-3.3.1.min.js">
+	</script>
+	<!--Skript for jQuery 3.3.1 minified -->
+</body>
 
-    <div class="modal" id="loginModal">
-  		<div class="modal-content">
-  			<div class="modal-header">	<span class="closeBtn"></span>
-  				<h2>Logg inn</h2>
-  				<h6>Trykk på utsiden av boksen for å lukke</h6>
-  			</div>
-  			<!-- Slutt på modal-header div -->
-  			<div class="modal-body">
-  				<h4>Brukernavn(e-post)</h4>
-  				<input name="loggInnBrukernavn" type="text" value="">
-  				<br>
-  				<br>
-  				<h5>Passord</h5>
-  				<input name="loggInnPassord" type="password" value="">
-  			</div>
-  			<!-- Slutt på modal-body div -->
-  			<div class="modal-footer">
-  				<button class="loggInnKnapp" name="LoggInnBtn" type="button">Logg Inn</button>
-  			</div>
-  			<!-- Slutt på modal-footer div -->
-  		</div>
-  		<!-- Slutt på modal-content div -->
-  	</div>
-  	<!-- Slutt på loginModal div -->
-  	<div class="modal" id="registrerModal">
-  		<div class="modal-content">
-  			<div class="modal-header">	<span class="closeBtn"></span>
-  				<h2>Registrer deg</h2>
-  				<h6>Trykk på utsiden av boksen for å lukke</h6>
-  			</div>
-  			<!-- Slutt på modal-header div -->
-  			<div class="modal-body">
-  				<h4>Brukernavn(e-post)</h4>
-  				<input name="RegistrerBrukernavn" type="text" value="">
-  				<br>
-  				<h4>Epost</h4>
-  				<input name="RegistrerBrukernavn" type="text" value="">
-  				<br>
-  				<h4>Passord</h4>
-  				<input name="RegistrerPassord" type="password" value="">
-  				<br>
-  				<h4>Passord igjen</h4>
-  				<input name="RegistrerPassordIgjen" type="password" value="">
-  			</div>
-  			<!-- Slutt på modal-body div -->
-  			<div class="modal-footer">
-  				<button class="RegKnapp" name="RegKnapp" type="button">Registrer deg</button>
-  			</div>
-  			<!-- Slutt på modal-footer div -->
-  		</div>
-  		<!-- Slutt på modal-content div -->
-  	</div>
-  	<!-- Slutt på registrerModal div -->
-
-    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8" crossorigin="anonymous"></script>
-  	<!--Skript for jQuery 3.3.1 minified -->
-  </body>
 </html>
