b5edf774e58a8bef83c5a2e6d11e35b6808ec92a
Kristian Stang <kristian.sta@gmail.com>
2018-02-13 15:41:49 +0100

    test

--- utils/database.php
+++ utils/database.php
@@ -2,25 +2,11 @@
 
 class Database {
 
-  private static $hostname = "localhost";
-  private static $username = "v18u130";
-  private static $password = "Pw130";
-  private static $database = "v18db130";
-  private static $tilkobling = null;
-
-  private static function kobleTil() {
-    self::$tilkobling = mysqli_connect($hostname, $username, $password, $database);
-  }
-
-  private static function kobleFra() {
-    self::$tilkobling->close();
-  }
-
-  public static function spørring() {
-    self::kobleTil();
-
-    self::kobleFra();
-  }
+  private $hostname = "localhost";
+  private $username = "v18u130";
+  private $password = "Pw130";
+  private $database = "v18db130";
+  private $tilkobling = null;
 
 }
 
