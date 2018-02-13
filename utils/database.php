70ff50e9fd1d9227ce520a2eb9a8344385da07bd
Kristian Stang <kristian.sta@gmail.com>
2018-02-13 14:53:14 +0100

    databasetilkobling

--- utils/database.php
+++ utils/database.php
@@ -1,11 +1,29 @@
 <?php
 
-  function getConnection() {
-    $hostname = "localhost";
-    $username = "v18u130";
-    $password = "Pw130";
-    $database = "v18db130";
-    return mysqli_connect($hostname, $username, $password, $database);
+class Database {
+
+  private static $hostname = "itfag.usn.no";
+  private static $username = "v18u130";
+  private static $password = "Pw130";
+  private static $database = "v18db130";
+  private static $tilkobling = null;
+
+  private static function kobleTil() {
+    self::$tilkobling = mysqli_connect($hostname, $username, $password, $database);
   }
+
+  private static function kobleFra() {
+    self::$tilkobling->close();
+  }
+
+  public static function spørring() {
+    self::kobleTil();
+    var_dump(self::$tilkobling);
+    self::kobleFra();
+  }
+
+}
+
+Database::spørring();
 
 ?>
