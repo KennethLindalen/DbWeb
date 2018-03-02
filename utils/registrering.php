66cf061bbb07650f10309cdd1829e68730499957
Kristian Stang <kristian.sta@gmail.com>
2018-03-02 12:36:25 +0100

    logikk for registrering

--- utils/registrering.php
+++ utils/registrering.php
@@ -1,5 +1,22 @@
 <?php
 
+  include_once "models/Medlem.php";
 
+  $feil = [];
+
+  if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    try {
+      $medlem = new Medlem($_POST);
+      $medlem->lagre();
+    }
+    catch (InvalidArgumentException $e) {
+      $feil = json_decode($e->getMessage(), true);
+    }
+    catch (mysqli_sql_exception $e) {
+      header("Location: error.php");
+    }
+
+  }
 
 ?>
