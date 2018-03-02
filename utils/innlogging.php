6de845cef3a8e3a9b51620ece45eb62baf802fc1
Kristian Stang <kristian.sta@gmail.com>
2018-03-02 12:36:34 +0100

    logikk for innlogging

--- utils/innlogging.php
+++ utils/innlogging.php
@@ -1,5 +1,24 @@
 <?php
 
+include_once "utils/session.php";
+include_once "models/Medlem.php";
 
+$feil = [];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+  try {
+    $medlemsnummer = Medlem::autentiser($_POST["identifikator"], $_POST["passord"]);
+    $_SESSION["medlemsnummer"] = $medlemsnummer;
+    $_SESSION["erInlogget"] = true;
+  }
+  catch (InvalidArgumentException $e) {
+    $feil = json_decode($e->getMessage(), true);
+  }
+  catch (mysqli_sql_exception $e) {
+    header("Location: error.php");
+  }
+
+}
 
 ?>
