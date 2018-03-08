b380e5936cbe96a4c9eb5d2a38e0449c251f8d01
svgnAyure <kristian.sta@gmail.com>
2018-03-08 05:35:40 +0100

    medlemmenes "min side"

---
+++ minside.php
@@ -0,0 +1,16 @@
+<?php include_once "utils/beskyttet.php"; ?>
+
+<!DOCTYPE html>
+<html>
+	<?php include "includes/head.php"; ?>
+	<body>
+		<?php include "includes/header.php"; ?>
+		<?php include "includes/navbar.php"; ?>
+		<main>
+			<div class="container">
+				<h1>Min side</h1>
+			</div>
+		</main>
+		<?php include "includes/footer.php"; ?>
+	</body>
+</html>
