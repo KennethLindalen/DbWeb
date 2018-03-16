528721135657814359e2c607f5c1f51b65bcd980
Kristian Stang <kristian.sta@gmail.com>
2018-03-16 13:47:34 +0100

    La til kommentarer

--- models/Medlem.php
+++ models/Medlem.php
@@ -1,9 +1,15 @@
 <?php
 
+// Includes
 include_once "utils/database.php";
 
+// Klasse som representerer klubbens medlemmer
 class Medlem {
 
+  // Konstruktørmetode
+  // Medlemsobjekter opprettes direkte fra $_POST ved registrering, eller fra
+  // et assosiativt array som hentes fra eksisterende oppføringer i databasen.
+  // Ulike objektvariabler benyttes basert på hvor dataene kommer fra.
   public function __construct($medlem = [], $fraDatabase = false) {
     $this->medlemsnummer = $fraDatabase ? $medlem["medlemsnummer"] : null;
     $this->fornavn       = $medlem["fornavn"];
@@ -18,80 +24,119 @@ class Medlem {
     if (!$fraDatabase) $this->valider();
   }
 
+  // Metode for validering av felter
+  // Utføres kun når medlemsobjektet konstrueres fra $_POST ved registrering,
+  // altså stoler vi på dataene dersom de kommer direkte fra databasen.
   private function valider() {
     $feil = [];
 
+    // Fornavn kan bestå av bokstaver, bindestrek, apostrof og punktum. Maks 100 tegn.
     if (!preg_match("/^[\pL\s'.-]{1,100}$/", $this->fornavn))
       $feil["fornavn"] = "Ugyldig fornavn";
 
+    // Etternavn kan bestå av bokstaver, bindestrek, apostrof og punktum. Maks 100 tegn.
     if (!preg_match("/^[\pL\s'.-]{1,100}$/", $this->etternavn))
       $feil["etternavn"] = "Ugyldig etternavn";
 
+    // Fornavnet kan kun bestå av bokstaver, tall, bindestrek, apostrof, komma og punktum. Maks 100 tegn.
     if (!preg_match("/^[\pL\s\d'.,-]{1,100}$/", $this->adresse))
       $feil["adresse"] = "Ugyldig adresse";
 
+    // Postnummeret må bestå av 4 siffer
     if (!preg_match("/^\d{4}$/", $this->postnummer))
       $feil["postnummer"] = "Ugyldig postnummer";
 
+    // Telefonnummer må bestå av 8 siffer. Vi begrenser oss altså til norske telefonnummer.
     if (!preg_match("/^\d{8}$/", $this->telefonnummer))
       $feil["telefonnummer"] = "Ugyldig telefonnummer";
 
+    // Bruker PHP sitt innebygde filter for å validere e-postadresse. Maks 100 tegn.
     if (!filter_var($this->epost, FILTER_VALIDATE_EMAIL) || strlen($this->epost) > 100)
       $feil["epost"] = "Ugyldig e-postadresse";
 
+    // Passordet må inneholde store- og små bokstaver og tall. Minst 8 tegn.
     if (!preg_match("/(?=.*\d)(?=.*[a-zæøå])(?=.*[A-ZÆØÅ]).{8,}/", $this->passord))
       $feil["passord"] = "Passordet må bestå av minst 8 tegn og inneholde både tall, store-, og små bokstaver";
 
+    // Bruker må oppgi passord to ganger for å sikre at han/hun ikke har tastet feil
     if ($this->passord !== $this->passord2)
       $feil["passord2"] = "Passordene må være like";
 
+    // Dersom noen av valideringene feilet, kast unntak og send med forklaringer.
     if (!empty($feil))
       throw new InvalidArgumentException(json_encode($feil));
 
+    // Dersom valideringene er godkjent, krypter passord og fjern ukryptert fra objekt.
     $this->passord = password_hash($this->passord, PASSWORD_BCRYPT);
     $this->passord2 = null;
   }
 
+  // Metode for å lagre et medlemsobjekt til databasen.
   public function lagre() {
+
+    // Feil kan oppstå ved blant annet tilkobling til databasen og ved
+    // utførelsen av de aktuelle spørringene, derfor try-catch-blokk.
     try {
+
+      // Oppdatering av eksisterende medlem hvis medlemsnummer allerede er definert
       if ($this->medlemsnummer)
         $this->oppdater();
+
+      // Innsetting av nytt medlem dersom objektet ikke allerede har medlemsnummer.
       else
         $this->settInn();
     }
+
+    // Vi filtrerer ut noen av feilene som kan oppstå som er relatert til validering av inndata.
     catch (mysqli_sql_exception $e) {
+
+      // Feilkode 1062 - brudd på UNIQUE i databasen - e-postadressen eksisterer.
       if ($e->getCode() == 1062)
         throw new InvalidArgumentException(json_encode(["epost" => "E-postadressen er allerede i bruk"]));
+
+      // Feilkode 1452 - brudd på fremmednøkkelkrav i databasen - ugyldig postnummer.
       if ($e->getCode() == 1452)
         throw new InvalidArgumentException(json_encode(["postnummer" => "Ugyldig postnummer"]));
+
+      // Kast unntaket videre dersom det ikke er relatert til validering.
       throw $e;
     }
   }
 
+  // Metode for innsetting av nye medlemmer.
   private function settInn() {
+
+    // SQL-spørring med parametre for bruk i prepared statement.
     $sql = "
       INSERT INTO medlem (fornavn, etternavn, adresse, postnummer, telefonnummer, epost, passord)
       VALUES (?, ?, ?, ?, ?, ?, ?);
     ";
 
+    // Verdiene som skal settes inn i databasen.
     $verdier = [
       $this->fornavn,
       $this->etternavn,
-      $this->adresse, 
-      $this->postnummer, 
-      $this->telefonnummer, 
-      $this->epost, 
+      $this->adresse,
+      $this->postnummer,
+      $this->telefonnummer,
+      $this->epost,
       $this->passord
     ];
 
+    // Kobler til databasen og utfører spørringen.
     $con = new Database();
     $res = $con->spørring($sql, $verdier);
 
+    // Hent medlemsnummer fra databasen (autoincrement) og knytt til objektet.
+    // Passordet fjernes fra objektet for sikkerhets skyld.
     $this->medlemsnummer = $res->insert_id;
     $this->passord = null;
   }
 
+  // Metode for oppdatering av eksisterende medlemmer.
   private function oppdater() {
+
+    // SQL-spørring med parametre for bruk i prepared statement.
     $sql = "
       UPDATE medlem
       SET
@@ -105,6 +150,7 @@ class Medlem {
         medlemsnummer = ?
     ";
 
+    // Verdiene som skal settes inn i databasen.
     $verdier = [
       $this->fornavn,
       $this->etternavn,
@@ -115,11 +161,15 @@ class Medlem {
       $this->medlemsnummer
     ];
 
+    // Kobler til databasen og utfører spørringen.
     $con = new Database();
     $res = $con->spørring($sql, $verdier);
   }
 
+  // Statisk metode for å finne et medlem basert på gitt medlemsnummer.
   public static function finn($medlemsnummer) {
+
+    // SQL-spørring med parametre for bruk i prepared statement.
     $sql = "
       SELECT
         m.medlemsnummer,
@@ -138,34 +188,45 @@ class Medlem {
         medlemsnummer = ?;
     ";
 
+    // Kobler til databasen og utfører spørringen.
+    // Henter resultatet fra spørringen i et assosiativt array ($res).
     $con = new Database();
     $res = $con
       ->spørring($sql, [$medlemsnummer])
       ->get_result()
       ->fetch_assoc();
 
+    // Returnerer et nytt medlemsobjekt.
     return new Medlem($res, true);
   }
 
+  // Statisk metode for autentisering av brukere ved innlogging.
   public static function autentiser($identifikator, $passord) {
+
+    // SQL-spørring med parametre for bruk i prepared statement.
     $sql = "
       SELECT medlemsnummer, passord
       FROM medlem
       WHERE medlemsnummer = ? OR epost = ?;
     ";
 
+    // Kobler til databasen og utfører spørringen.
+    // Henter resultatet fra spørringen i et assosiativt array ($res).
     $con = new Database();
     $res = $con
       ->spørring($sql, [$identifikator, $identifikator])
       ->get_result()
       ->fetch_assoc();
 
+    // Verifiserer passordet ved å sammenlikne brukerinput og hash fra databasen.
     if (password_verify($passord, $res["passord"]))
       return $res["medlemsnummer"];
 
+    // Kast unntak dersom autentiseringen feilet og gi passende tilbakemelding.
     throw new InvalidArgumentException(json_encode(["autentisering" => "Autentisering feilet"]));
   }
 
+  // Metode for omgjøring av et medlemsobjekt til et assosiativt array uten null-verdier.
   public function toArray() {
     return array_filter((array) $this, function($var) { return $var != null; });
   }
