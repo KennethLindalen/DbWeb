190c218c578bc9621672b72e7d400e2877ce6c50
Kristian Stang <kristian.sta@gmail.com>
2018-03-16 12:56:59 +0100

    endret testside

--- registrering.php
+++ registrering.php
@@ -10,29 +10,29 @@
   <body>
 
     <form method="post">
-      <input type="text" name="fornavn" placeholder="Fornavn" value="<?= fraArray($_POST, 'fornavn') ?>">
-      <?= fraArray($feil, "fornavn") ?><br>
+      <input type="text" name="fornavn" placeholder="Fornavn" value="<?= $_POST['fornavn'] ?? ""?>">
+      <?= $feil["fornavn"] ?? "" ?><br>
 
-      <input type="text" name="etternavn" placeholder="Etternavn" value="<?= fraArray($_POST, 'etternavn') ?>">
-      <?= fraArray($feil, "etternavn") ?><br>
+      <input type="text" name="etternavn" placeholder="Etternavn" value="<?= $_POST['etternavn'] ?? "" ?>">
+      <?= $feil["etternavn"] ?? "" ?><br>
 
-      <input type="text" name="adresse" placeholder="Adresse" value="<?= fraArray($_POST, 'adresse') ?>">
-      <?= fraArray($feil, "adresse") ?><br>
+      <input type="text" name="adresse" placeholder="Adresse" value="<?= $_POST['adresse'] ?? "" ?>">
+      <?= $feil["adresse"] ?? "" ?><br>
 
-      <input type="text" name="postnummer" placeholder="Postnummer" value="<?= fraArray($_POST, 'postnummer') ?>">
-      <?= fraArray($feil, "postnummer") ?><br>
+      <input type="text" name="postnummer" placeholder="Postnummer" value="<?= $_POST['postnummer'] ?? "" ?>">
+      <?= $feil["postnummer"] ?? "" ?><br>
 
-      <input type="text" name="telefonnummer" placeholder="Telefonnummer" value="<?= fraArray($_POST, 'telefonnummer') ?>">
-      <?= fraArray($feil, "telefonnummer") ?><br>
+      <input type="text" name="telefonnummer" placeholder="Telefonnummer" value="<?= $_POST['telefonnummer'] ?? "" ?>">
+      <?= $feil["telefonnummer"] ?? "" ?><br>
 
-      <input type="email" name="epost" placeholder="E-postadresse" value="<?= fraArray($_POST, 'epost') ?>">
-      <?= fraArray($feil, "epost") ?><br>
+      <input type="email" name="epost" placeholder="E-postadresse" value="<?= $_POST['epost'] ?? "" ?>">
+      <?= $feil["epost"] ?? "" ?><br>
 
-      <input type="password" name="passord" placeholder="Passord" value="<?= fraArray($_POST, 'passord') ?>">
-      <?= fraArray($feil, "passord") ?><br>
+      <input type="password" name="passord" placeholder="Passord" value="<?= $_POST['passord'] ?? "" ?>">
+      <?= $feil["passord"] ?? "" ?><br>
 
-      <input type="password" name="passord2" placeholder="Gjenta passord" value="<?= fraArray($_POST, 'passord2') ?>">
-      <?= fraArray($feil, "passord2") ?><br>
+      <input type="password" name="passord2" placeholder="Gjenta passord" value="<?= $_POST['passord2'] ?? "" ?>">
+      <?= $feil["passord2"] ?? "" ?><br>
 
       <button>Send</button>
     </form>
