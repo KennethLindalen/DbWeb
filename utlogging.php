58155f2cd3e77c2ea1a3ec2d14e20f6f4e842aa6
Kristian Stang <kristian.sta@gmail.com>
2018-03-16 14:38:27 +0100

    La til kommentarer

--- utlogging.php
+++ utlogging.php
@@ -1,7 +1,12 @@
 <?php
 
+// Modul for utlogging av brukere.
+
+// Må ha tilgang til sesjonsvariabler.
 include_once "utils/session.php";
 
+// Fjerner medlemsnummeret fra sesjonen - bruker logges ut.
+// Redirigerer deretter til hovedsiden.
 $_SESSION["medlemsnummer"] = false;
 header("Location: /");
 
