18b62cc8876555be7c3d59f2fe84d3fcdb419afc
Kristian Stang <kristian.sta@gmail.com>
2018-03-16 12:56:09 +0100

    la til enkel brukerinfo for testing

--- minside.php
+++ minside.php
@@ -1,4 +1,6 @@
 <?php include_once "utils/beskyttet.php"; ?>
+<?php include_once "models/Medlem.php"; ?>
+<?php $medlem = Medlem::finn($_SESSION["medlemsnummer"]);	?>
 
 <!DOCTYPE html>
 <html>
@@ -9,6 +11,17 @@
 		<main>
 			<div class="container">
 				<h1>Min side</h1>
+				<div>
+					<p>Medlemsnummer: <?= $medlem->medlemsnummer; ?></p>
+					<p>Fornavn: <?= $medlem->fornavn; ?></p>
+					<p>Etternavn: <?= $medlem->etternavn; ?></p>
+					<p>Adresse: <?= $medlem->adresse; ?></p>
+					<p>Postnummer: <?= $medlem->postnummer; ?></p>
+					<p>Poststed: <?= $medlem->poststed; ?></p>
+					<p>Telefonnummer: <?= $medlem->telefonnummer; ?></p>
+					<p>E-postadresse: <?= $medlem->epost; ?></p>
+				</div>
+
 			</div>
 		</main>
 		<?php include "includes/footer.php"; ?>
