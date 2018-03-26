e9664e9ba8ef6a3268fd9ea25bc7111454827c52
svgnAyure <kristian.sta@gmail.com>
2018-03-26 06:26:04 +0200

    La til nye, foreløpig tomme, sidefiler.

---
+++ bestilling.php
@@ -0,0 +1,8 @@
+<?php
+  $tittel = "Banebestilling - Nederlaget Idrettsklubb";
+  include "layout/førInnhold.php";
+?>
+<!-- Innhold starter her -->
+
+<!-- Innhold stopper her -->
+<?php include "layout/etterInnhold.php"; ?>
---
+++ faktura.php
@@ -0,0 +1,9 @@
+<?php
+  $tittel = "Faktura - Nederlaget Idrettsklubb";
+  include "layout/førInnhold.php";
+  include "utils/beskyttet.php";
+?>
+<!-- Innhold starter her -->
+
+<!-- Innhold stopper her -->
+<?php include "layout/etterInnhold.php"; ?>
