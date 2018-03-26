e98c0796dc2a48847698ab5f036a305f621acb19
svgnAyure <kristian.sta@gmail.com>
2018-03-26 06:23:35 +0200

    La til layout-filer som inkluderes på sidene.

---
+++ layout/førInnhold.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include "includes/head.php" ?>
+  <body class="bg-light" style="overflow-y: scroll;">
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-3 pl-0 pr-4">
+          <?php include "includes/header.php"; ?>
+          <?php include "includes/navbar.php"; ?>
+        </div>
+        <div class="col-9">
