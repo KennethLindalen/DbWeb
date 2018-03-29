05c7908e198ea8d633a61e11002b0368382e2a3c
svgnAyure <kristian.sta@gmail.com>
2018-03-29 06:49:21 +0200

    Endret standard fontstørrelse (midlertidig?).

--- layout/førInnhold.php
+++ layout/førInnhold.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <?php include "includes/head.php" ?>
-  <body class="bg-light" style="overflow-y: scroll;">
+  <body class="bg-light" style="overflow-y: scroll; font-size: 0.9rem;">
     <div class="container my-5">
       <div class="row">
         <div class="col-3 pl-0 pr-4">
