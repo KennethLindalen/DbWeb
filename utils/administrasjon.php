90b01ea40622aca4b119ff604f102958f3932673
svgnAyure <kristian.sta@gmail.com>
2018-03-29 06:44:11 +0200

    Logikk vedrørende administrasjonssiden.

---
+++ utils/administrasjon.php
@@ -0,0 +1,59 @@
+<?php
+
+include_once "models/Medlem.php";
+include_once "models/Idrett.php";
+include_once "models/Anlegg.php";
+include_once "models/Reservasjon.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+  $feil = [];
+
+  try {
+
+    
+    switch ($_POST["operasjon"]) {
+
+      //
+      case "slettMedlem":
+        Medlem::slett($_POST["medlemsnummer"]);
+        break;
+
+      //
+      case "nyIdrett":
+        $idrett = new Idrett($_POST);
+        $idrett->lagre();
+        break;
+
+      //
+      case "slettIdrett":
+        Idrett::slett($_POST["idrettskode"]);
+        break;
+
+      //
+      case "nyttAnlegg":
+        break;
+
+      //
+      case "slettAnlegg":
+        break;
+
+      //
+      case "slettReservasjon":
+        break;
+    }
+
+  }
+
+  catch (InvalidArgumentException $e) {
+    $feil = json_decode($e->getMessage(), true);
+  }
+
+  catch (mysqli_sql_exception $e) {
+    header("Location: error.php?id=1");
+  }
+
+}
+
+
+?>
