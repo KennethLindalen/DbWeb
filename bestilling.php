9ff6391aa82d5569f4092aa1262f14389f0dd37b
svgnAyure <kristian.sta@gmail.com>
2018-04-03 01:34:59 +0200

    Fjernet gamle filer.

--- bestilling.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $tittel = "Banebestilling - Nederlaget Idrettsklubb";
-  include "layout/førInnhold.php";
-?>
-<!-- Innhold starter her -->
-
-<!-- Innhold stopper her -->
-<?php include "layout/etterInnhold.php"; ?>
--- faktura.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $tittel = "Faktura - Nederlaget Idrettsklubb";
-  include "layout/førInnhold.php";
-  include "utils/krevMedlem.php";
-?>
-<!-- Innhold starter her -->
-
-<!-- Innhold stopper her -->
-<?php include "layout/etterInnhold.php"; ?>
