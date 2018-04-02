3bf1ed98ef718f20311cda062721251af416df7f
svgnAyure <kristian.sta@gmail.com>
2018-04-03 01:36:43 +0200

    Metoder for trygg output av brukerinput.

--- utils/funksjoner.php
+++ utils/funksjoner.php
@@ -2,8 +2,14 @@
 
 // Denne filen inneholder generelle funksjoner og hjelpemidler til bruk overalt på siden.
 
+// Henter ut og renser en verdi fra et array dersom den eksisterer.
 function fraArray($arr, $felt) {
-  return isset($arr[$felt]) ? htmlspecialchars($arr[$felt]) : "";
+  return isset($arr[$felt]) ? rens($arr[$felt]) : "";
+}
+
+// Renser output for potensielle skadelige tekststrenger i HTML-sammenheng.
+function rens($string) {
+  return htmlspecialchars($string);
 }
 
 ?>
