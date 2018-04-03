657f9c9b91eab56992c6df65b2ac922b04d4921e
svgnAyure <kristian.sta@gmail.com>
2018-04-03 21:41:34 +0200

    La til støtte for enkel validering via JS.

--- registrering.php
+++ registrering.php
@@ -20,7 +20,7 @@
       <div class="form-group col-6">
         <label for="fornavn">Fornavn</label>
         <input
-          type="text" class="form-control <?= erGyldig("fornavn") ?>"
+          type="text" class="form-control valideres <?= erGyldig("fornavn") ?>"
           name="fornavn" id="fornavn" placeholder="Fornavn"
           value="<?= fraArray($_POST, "fornavn") ?>">
         <div class="invalid-feedback"><?= fraArray($feil, "fornavn") ?></div>
@@ -29,7 +29,7 @@
       <div class="form-group col-6">
         <label for="etternavn">Etternavn</label>
         <input
-          type="text" class="form-control <?= erGyldig("etternavn") ?>"
+          type="text" class="form-control valideres <?= erGyldig("etternavn") ?>"
           name="etternavn" id="etternavn" placeholder="Etternavn"
           value="<?= fraArray($_POST, "etternavn") ?>">
         <div class="invalid-feedback"><?= fraArray($feil, "etternavn") ?></div>
@@ -42,7 +42,7 @@
       <div class="form-group col-12">
         <label for="adresse">Adresse</label>
         <input
-          type="text" class="form-control <?= erGyldig("adresse") ?>"
+          type="text" class="form-control valideres <?= erGyldig("adresse") ?>"
           name="adresse" id="adresse" placeholder="Adresse"
           value="<?= fraArray($_POST, "adresse") ?>">
         <div class="invalid-feedback"><?= fraArray($feil, "adresse") ?></div>
@@ -55,7 +55,7 @@
       <div class="form-group col-4">
         <label for="postnummer">Postnummer</label>
         <input
-          type="text" class="form-control <?= erGyldig("postnummer") ?>"
+          type="text" class="form-control valideres <?= erGyldig("postnummer") ?>"
           name="postnummer" id="postnummer" placeholder="Postnummer"
           value="<?= fraArray($_POST, "postnummer") ?>">
         <div class="invalid-feedback"><?= fraArray($feil, "postnummer") ?></div>
@@ -73,7 +73,7 @@
       <div class="form-group col-12">
         <label for="telefonnummer">Telefonnummer</label>
         <input
-          type="phone" class="form-control <?= erGyldig("telefonnummer") ?>"
+          type="phone" class="form-control valideres <?= erGyldig("telefonnummer") ?>"
           name="telefonnummer" id="telefonnummer" placeholder="Telefonnummer"
           value="<?= fraArray($_POST, "telefonnummer") ?>">
         <div class="invalid-feedback"><?= fraArray($feil, "telefonnummer") ?></div>
@@ -86,7 +86,7 @@
       <div class="form-group col-12">
         <label for="epost">E-postadresse</label>
         <input
-          type="email" class="form-control <?= erGyldig("epost") ?>"
+          type="email" class="form-control valideres <?= erGyldig("epost") ?>"
           name="epost" id="epost" placeholder="E-postadresse"
           value="<?= fraArray($_POST, "epost") ?>">
         <div class="invalid-feedback"><?= fraArray($feil, "epost") ?></div>
@@ -99,7 +99,7 @@
       <div class="form-group col-6">
         <label for="passord">Passord</label>
         <input
-          type="password" class="form-control <?= erGyldig("passord") ?>"
+          type="password" class="form-control valideres <?= erGyldig("passord") ?>"
           name="passord" id="passord" placeholder="Passord">
         <div class="invalid-feedback"><?= fraArray($feil, "passord") ?></div>
       </div>
@@ -107,7 +107,7 @@
       <div class="form-group col-6">
         <label for="passord2">Gjenta passord</label>
         <input
-          type="password" class="form-control <?= erGyldig("passord2") ?>"
+          type="password" class="form-control valideres <?= erGyldig("passord2") ?>"
           name="passord2" id="passord2" placeholder="Gjenta passord">
         <div class="invalid-feedback"><?= fraArray($feil, "passord2") ?></div>
       </div>
