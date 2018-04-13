eff29c808819a56d808ad63119104cf12901f5d9
Hans <23290899+cghansell@users.noreply.github.com>
2018-04-13 10:21:16 +0200

    la inn (en) råtekst om klubbens aktiviteter

--- aktiviteter.php
+++ aktiviteter.php
@@ -2,7 +2,17 @@
   $tittel  = "Aktiviteter - Nederlaget Idrettsklubb";
   include "layout/førInnhold.php";
 ?>
-<!-- Innhold starter her -->
+<!--
+Orientering - Orienteringslaget vårt har fortsatt ikke kommet tilbake fra NM
+Fotball     - Seniorlaget vårt spiller i 6 divisjon avdeling 9
+Bordtennis  - Et nytt tilbud vi har for våre medlemmer.
+Tennis      - Kontakt Cedric-Conrad Kjoos(ja han har Tesla) for informasjon.
+
+Medlemmer kan benytte seg av vårt splitter nye edb - system for booking av baner,
+sjekke terminlister med mer - bli medlem og bli en del av Nederlaget du også.
+
+
+ -->
 
 <h1 class="display-4">Aktiviteter</h1>
 <hr>
