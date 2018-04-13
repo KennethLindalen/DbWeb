62e7f36dd2d0909f0a30c9a9a1562dcedaec78db
Hans <23290899+cghansell@users.noreply.github.com>
2018-04-13 10:16:21 +0200

    Add files via upload
    la inn tekst og historikk om nederlaget il

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
   include "layout/førInnhold.php";
 ?>
 <!-- Innhold starter her -->
+Nederlaget Idrettsklubb ble grunnlagt av Rigmor og Kolbjørn Berggylte
+i 1958. Meningen var at Nederlaget skulle være en lavterskel
+breddeklubb som tilbød sambygdinger konkurranse og trivsel i skjønn forening.
+Fra oppstarten tilbød klubben idretter som kappe land, boksen går og
+gjemsel helt til 23-årige Sivert Grum gjemte seg så grundig under Gøymekappleiken Askim 1979
+at han ikke ble funnet før sauesankinga på Shetland i 1985. Etter dette fikk Nederlaget
+egen HMS-ansvarlig og ble tatt opp i Norges Idrettsforbund under forutsetning at
+klubben skulle tilby "normale" idretter til sine medlemmer. Klubben består av
+i dag alt mellom 15-250 medlemmer og tilbyr idretter som Tennis, Bordtennis og
+Orientering.
+
 
 <!-- Innhold stopper her -->
 <?php include "layout/etterInnhold.php"; ?>
